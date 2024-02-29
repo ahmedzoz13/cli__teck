d53a87693789bb88b0ac14e4f44215f009bcfddb
ahmedzoz13 <158785400+ahmedzoz13@users.noreply.github.com>
2024-02-29 22:02:00 +0200

    commit msg

---
+++ routes/api.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);    
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware(['Jwt.verify'])->group(function () {
+
+    Route::get('/posts',[PostController::class,'index']);
+    Route::get('/post/{id}',[PostController::class,'show']);
+    Route::post('/posts',[PostController::class,'store']);
+    Route::post('/post/{id}',[PostController::class,'update']);
+    Route::post('/posts/{id}',[PostController::class,'destroy']);
+
+});
